ed3e1893a24f60b0e99f6b449dd91d0d0395c11a
Vedat Tavas <vtavas@hotmail.com>
2020-07-26 13:44:56 +0300

    Ex 3.11 - Frontend's static codes added to backend

---
+++ build/precache-manifest.83881657d9881633942bd2c3dd752088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b665b540474cecfa8650ef248d7dc12d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e945d927bee1aba6b811",
+    "url": "/static/css/main.b5adea53.chunk.css"
+  },
+  {
+    "revision": "c164db0d716c10a5f408",
+    "url": "/static/js/2.f7f5c33f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7f5c33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e945d927bee1aba6b811",
+    "url": "/static/js/main.f015a2fa.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
